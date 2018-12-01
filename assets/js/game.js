260f91aa439d694b2ef9bff945dc6a09de7baeb5
johndoeadams <johndoeadams@gmail.com>
2018-12-01 15:34:17 -0500

    stuff

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,9 +6,28 @@ var game={
   "score":0,
   "wins":0,
   "lose":0,
-  "word":""
+  "word":"",
+  "guess":[]
 }; //End of game definition
 
+var used=[];
+
+var temp=["habit",
+"happily",
+"Harry" ,
+"heading" ,
+"hunter" ,
+"Illinois" ,
+"image",
+"independent",
+"instant",
+"January" ,
+"kids" ,
+"label" ,
+"Lee",
+"lungs",
+"manufacturing"
+];
 
 //GAME LOGIC
 
@@ -18,8 +37,13 @@ function checkLetter(x){
 var regexp=/[qwertyuiopasdfghjklzxcvbnm]/;
 //input must be a valid character and must be a string
   if(regexp.test(x) && typeOf(x)=="string"){
-    //pass the letter to the checkWord method
-    checkWord(x);
+    //pass the letter to the checkWord method if its not already used
+    if(used.!contains(x)){
+      checkWord(x);
+    }
+    else{
+      used.push(x);
+    }
   }
 }
 //checks the user selected letter against the current word
@@ -27,16 +51,29 @@ function checkWord(y){
   //Loop through the current "word" string
   for(var i=0; i<game["word"].length; i++){
     //check if letter is at the current index of the string
-    if(y==){
+    var letter=game["word"][i];
+    if(y==letter){
       //show letter on the screen
+
+    }
+    else if (game["lives"]>0) {
+      game["lives"]-=1;
+      //animate something
+    }
+    else{
+
     }
   }
 }
-//resets the game, generates a new game
-function resetGame(){
+//resets the game (continue)
+function resetGame(z){
 //reset Variables
+z["lives"]=6;
+z["word"]=temp[Math.floor(Math.random()*temp.length)];
+z["wins"]+=1;
 
-
-
+}
+//total reset of game
+function newGame(){
 
 }
